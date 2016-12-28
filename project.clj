30c61cd2523161dc521dde0ef3ddc060ca4f69a8
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-12-28 22:03:18 +0100

    Initial

---
+++ project.clj
@@ -0,0 +1,38 @@
+(defproject scarf "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :jvm-opts ^:replace ["-Xmx1g" "-server"]
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.293"]
+                 [org.clojure/core.async "0.2.395"]
+                 [org.omcljs/om "1.0.0-alpha47"]
+                 [ring "1.5.0"]
+                 [compojure "1.5.1"]
+                 [com.datomic/datomic-free "0.9.5544" :exclusions [joda-time]]]
+
+  :plugins [[lein-cljsbuild "1.1.5"]
+            [lein-figwheel "0.5.8"]]
+
+  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.8"]
+                                  [com.cemerick/piggieback "0.2.1"]]}}
+
+  :source-paths ["src/clj" "src/cljs"]
+  :resource-paths ["resources"]
+  :clean-targets ^{:protect false} ["resources/public/js/out"
+                                    "resources/public/js/main.js"]
+
+  :figwheel {:ring-handler scarf.core/handler}
+
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src/clj" "src/cljs"]
+                        :figwheel true
+                        :compiler {:output-to "resources/public/js/main.js"
+                                   :output-dir "resources/public/js/out"
+                                   :main scarf.core
+                                   :asset-path "js/out"
+                                   :optimizations :none
+                                   :source-map true}}]})
