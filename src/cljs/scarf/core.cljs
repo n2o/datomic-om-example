a51decbb9780242875a4e7b6592e68c28ef5871e
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-01-05 23:46:43 +0100

    Rewrite app to om next

--- src/cljs/scarf/core.cljs
+++ src/cljs/scarf/core.cljs
@@ -1,13 +1,44 @@
 (ns ^:figwheel-always scarf.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [cljs.core.async :as async :refer [put! chan alts!]]
-            [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
-
+  (:require [goog.dom :as gdom]
+            [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom :include-macros true]
+            [cemerick.url :refer [url]]
+            [scarf.views.contact :as cview]))
 (enable-console-print!)
-(println "Hello world!")
 
-(def app-state
-  (atom {:classes []}))
+;; ----------------------------------------------------------------------------
+;; Routes
+(def path (:path (url js/window.location)))
+path
+
+;;;;
+(def init-data
+  {:foo :bar})
+
+(defmulti read om/dispatch)
+(defmethod read :dashboard/items
+  [{:keys [state]} k _]
+  (let [st @state]
+    {:value (into [] (map #(get-in st %)) (get st k))}))
+
+(defmulti mutate om/dispatch)
+(defmethod mutate 'dashboard/favorite
+  [{:keys [state]} k {:keys [ref]}]
+  {:action
+   (fn [] (swap! state update-in (conj ref :favorites) inc))})
+
+(def reconciler
+  (om/reconciler
+    {:state  init-data
+     :parser (om/parser {:read read :mutate mutate})}))
 
 
+(cond
+  (= "/kontakt" path) (om/add-root! reconciler cview/contact-view (gdom/getElement "app"))
+  (= "/" path) (println "index") 
+  )
+
+
+(defn on-js-reload []
+
+)
